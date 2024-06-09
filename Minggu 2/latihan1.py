1eaa9655e3f2194b566b06dbbb4ef62170a64222
IndraFadillahCode <112888476+IndraFadillahCode@users.noreply.github.com>
2024-06-09 22:45:35 +0700

    Add files via upload

---
+++ Minggu 2/latihan1.py
@@ -0,0 +1,142 @@
+
+
+print("Latihan 1")
+print("Hello World!@#!#!$")
+print("test")
+
+print()
+
+print("Latihan 2")
+print("Hello, Python!")
+print("Indra fadillah")
+print('Indra fadillah')
+
+print()
+ 
+print("Latihan 3")
+print("Saya bangun pagi\n saya malam tidur") # \n berfungsi untuk enter
+print("Saya bangun pagi\t saya malam tidur")
+
+print()
+
+print("nama saya \"Indra\"") # \ berfungsi untuk escaping character
+print()
+
+print("\"")
+print()
+
+print("satu.","dua.","tiga.")
+print()
+
+print("Nama saya","indra", end=" ")
+print("Aditya")
+print()
+
+print("satu","dua","tiga", sep=", ")
+print()
+
+print("Latihan 4")
+#sebelumnya 
+#print("Programming","Essentials","in")
+#print("Python")
+#diubah menjadi : `Programming***Essentials***in...Python`
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python"*2)
+print("")
+
+print("latihan 5") 
+#sebelumnya ada 2 seperti ini didpu
+#print("    *    "*2)
+#print("   * *   "*2)
+#print("  *   *  "*2)
+#print(" *     * "*2)
+#print("***   ***"*2)
+#print("  *   *  "*2)
+#print("  *   *  "*2)
+#print("  *****  "*2)
+#lalu didupilkasi menjadi seperti ini : 
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
+print()
+
+print("Latihan 6")
+print("2"*2) #berisi angka string dan interger
+print(2*2) #ini berisi angka interger
+
+print(type("2")) #menjelaskan class string
+print(type(2)) #menjelaskan class interger 
+print()
+
+print("Latihan 7")
+print(5, "mempunyai tipe", type(5)) #ini tipe interger 
+print(.5, "mempunyai tipe", type(.5)) #ini tipe float
+print()
+
+print("Latihan 8")
+print(10_000_00) #float dipashkan dengan "." dan "," diberi "_" agar mudah dibaca
+print()
+
+print("Latihan 9")
+print("I'm Python")
+print()
+
+print("Latihan 10")
+print(True) #memberi tahu benar
+print(False) #memberi tahu salah
+
+print(4>2)
+print(3>4)
+print()
+
+print("Latihan 11")
+print()
+
+print("Latihan 12") #untuk menampilkan output yang terpisah tiap baris
+print('"I\'m"\n""learning""\n"""Python"""')
+
+print("""
+"I'm"
+""learning""
+\"""Python""\"
+""")
+print()
+
+print("Latihan 13") #ini merupakan operator aritmatika
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3.)
+print(2. ** 3)
+print()
+
+print(10 / 3)
+print(10 // 3)
+
+print(6 / 3.)
+print(6 // 3.)
+
+print(-4 + 4)
+print(-4. + 8)
+print()
+
+print("Latihan 14") #urutan operator 
+2 + 3 *5
+print(9 % 6 % 2)
+print(9 % 6)
+print(3 % 2)
+
+print(2 ** 2 ** 3)
+print(2 ** 2)
+print(2 ** 8)
+print((5 * ((25 % 13)+100) / (2 * 13)) // 2)
+print()
+
+print("Latihan 15") #variabel
+var = 1
+print(var)
+print()
+
+a=2. #untuk menulis rumus c=\sqrt{a^2+b^2}
+b=3.
+c=(a**2 + b**2) ** 0.5
+print("c = ", c)
+print()
+
